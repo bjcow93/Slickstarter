966601b0022758591d637e55393d1a896b725a70
Ben Cowden <benjcowden@gmail.com>
2019-02-09 20:19:19 -0800

    Update state in project_form

--- frontend/components/projects/project_form.jsx
+++ frontend/components/projects/project_form.jsx
@@ -19,6 +19,7 @@ class ProjectForm extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  
   update(property) {
     return e => this.setState({
       [property]: e.target.value
