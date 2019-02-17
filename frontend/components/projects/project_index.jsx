4a16197ddb68ace73c5a6c9329c687617210b701
Ben Cowden <benjcowden@gmail.com>
2019-02-16 20:19:19 -0800

    Comment out Route in project_index

--- frontend/components/projects/project_index.jsx
+++ frontend/components/projects/project_index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 import ProjectIndexItem from './project_index_item';
