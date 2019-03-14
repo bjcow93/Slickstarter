d58081eff754d39bfb1fb5bbf8faa9ef3e6485d0
Ben Cowden <benjcowden@gmail.com>
2019-03-13 21:27:23 -0700

    Delete debugger out of search controller

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,7 +1,6 @@
 class Api::SearchController < ApplicationController
   def filter
     @projects = Project.where("LOWER(title) LIKE '%#{params[:string].downcase}%'")
-# debugger
     render 'api/projects/index'
   end
 end
